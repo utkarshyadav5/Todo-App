d2c1f3775c1a44be4c48b68c152730c0944aba6a
Utkarsh <utkarsh.yadav5@gmail.com>
2017-07-03 14:51:19 +0530

    Added post method

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,11 +1,17 @@
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({extended: false});
+
+var data = [{item:'Do GFG'},{item:'Learn Machine Learning'},{item:'Start OS'},{item:'Start DBMS'}];
+
 module.exports = function(app){
 
     app.get('/todo',function(req,res){
-        res.render('todo');
+        res.render('todo', {todos: data});
     });
 
-    app.post('/todo',function(req,res){
-
+    app.post('/todo',urlencodedParser,function(req,res){
+        data.push(req.body);
+        res.json(data);
     });
 
     app.delete('/todo',function(req,res){
