2d3e358975e62a301b564f45b41d665d7bdcc661
Utkarsh <utkarsh.yadav5@gmail.com>
2017-07-03 20:53:07 +0530

    Added MongoDB package and included tit in todoController.js

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,6 +1,23 @@
 var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({extended: false});
 
+var mongoose = require('mongoose');
+
+//Connect to database
+mongoose.connect('mongodb://todoapp:todoapp@ds147052.mlab.com:47052/todo_app');
+
+//Create schema  - this is like a blueprint
+var todoSchema = new mongoose.Schema({
+    item: String
+});
+
+var Todo = mongoose.model('Todo',todoSchema);
+var itemOne = Todo({item:'Do puzzles'}).save(function(err){
+    if (err)
+        throw err;
+    console.log('item saved');
+});
+
 var data = [{item:'Do GFG'},{item:'Learn Machine Learning'},{item:'Start OS'},{item:'Start DBMS'}];
 
 module.exports = function(app){
